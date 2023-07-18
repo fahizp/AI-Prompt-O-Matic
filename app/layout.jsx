5777b952d12896023bf097bb5a97b6f02a5b1581
Muhammed Fahiz p <fahiz@Muhammeds-MacBook-Air.local>
2023-07-18 12:24:29 +0530

    Just Set The Project SetUp

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+import { Children } from "react";
+
+export const metadata = {
+  title: "AI-Prompt-O-Matic",
+  description: "Discover & Share AI Prompts",
+};
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"/>
+        </div>
+          <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
