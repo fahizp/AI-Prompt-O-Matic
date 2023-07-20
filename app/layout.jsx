bc0e9f61ac74353282c0cad634b7873074483511
Muhammed Fahiz p <fahiz@Muhammeds-MacBook-Air.local>
2023-07-20 15:01:50 +0530

    fix:.env added into the .gitignore

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "@styles/globals.css";
-import { Children } from "react";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "AI-Prompt-O-Matic",
@@ -9,13 +10,18 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <Provider>
         <div className="main">
           <div className="gradient" />
         </div>
-        <main className="app">{children}</main> 
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+        </Provider>
       </body>
     </html>
   );
 };
-  
+
 export default RootLayout;
