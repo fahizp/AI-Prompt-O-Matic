0b05bce92b1a76ef1bce2cdf975779917539166a
Muhammed Fahiz p <fahiz@Muhammeds-MacBook-Air.local>
2023-07-20 16:50:16 +0530

    feat:add the OAuth with nextAuth of react

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,21 @@
-const page = () => {
-  return <div>Home</div>;
+import Feed from "@components/Feed";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Explore & Distribute
+        <br className="max-md:hidden" />
+        <span className="new_gradient text-center">Smart Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        The AI-Prompt-O-Matic is an innovative AI-powered tool for today's
+        generation, enabling the exploration, generation, and exchange of
+        imaginative prompts.
+      </p>
+      <Feed/>
+    </section>
+  );
 };
 
-export default page;
+export default Home;
