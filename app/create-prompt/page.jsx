c874650246791b5ae64b0e6cfdacec045c6a0da8
Muhammed Fahiz p <fahiz@Muhammeds-MacBook-Air.local>
2023-07-21 16:13:13 +0530

    feat:add the create post to upload the prompt to the app

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,46 @@
+"use client";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
+
+const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [submitting, setsubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setsubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) router.push("/");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setsubmitting(false);
+    }
+  };
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
+
+export default CreatePrompt;
