e43d5b8130787d486dc5f9aa22c7414a5bb71776
Muhammed Fahiz p <fahiz@Muhammeds-MacBook-Air.local>
2023-07-22 15:43:16 +0530

    metadata: updated the icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,28 @@
 import "@styles/globals.css";
+import Head from "next/head";
+
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
   title: "AI-Prompt-O-Matic",
   description: "Discover & Share AI Prompts",
+  icons: {
+    icon: '/assets/images/favicon.ico',
+  },
 };
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
         <Provider>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
         </Provider>
       </body>
     </html>
