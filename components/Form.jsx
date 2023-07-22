d7883973303c0b6c0054b76efbe4f0e6d56da2b9
Muhammed Fahiz p <fahiz@Muhammeds-MacBook-Air.local>
2023-07-22 12:30:12 +0530

    fix: to this component to a resuable form componet for create post and edit post

--- components/Form.jsx
+++ components/Form.jsx
@@ -2,59 +2,59 @@ import Link from "next/link";
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <section className="w-full max-w-full flex-start flex-col">
-      <h1 className="head_text text-left">
-        <span className="create_gradient">{type} Post</span>
+    <section className='w-full max-w-full flex-start flex-col'>
+      <h1 className='head_text text-left'>
+        <span className='create_gradient'>{type} Post</span>
       </h1>
-      <p className="desc text-left max-w-md">
-        Generate and spread fascinating cues worldwide, unleashing your
-        imagination freely using any AI-powered platform.
+      <p className='desc text-left max-w-md'>
+        {type} and share amazing prompts with the world, and let your
+        imagination run wild with any AI-powered platform
       </p>
 
       <form
         onSubmit={handleSubmit}
-        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism "
+        className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
       >
         <label>
-          <span className="font-satoshi font-semibold text-base text-gray-700">
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
             Your AI Prompt
           </span>
 
           <textarea
             value={post.prompt}
             onChange={(e) => setPost({ ...post, prompt: e.target.value })}
-            placeholder="Write your post here"
+            placeholder='Write your post here'
             required
-            className="form_textarea "
+            className='form_textarea '
           />
         </label>
 
         <label>
-          <span className="font-satoshi font-semibold text-base text-gray-700">
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
             Field of Prompt{" "}
-            <span className="text-gray-500 text-sm">
-              (#image, #webdevelopment, #idea, etc.)
+            <span className='text-gray-500 text-sm'>
+              (#product, #webdevelopment, #idea, etc.)
             </span>
           </span>
           <input
             value={post.tag}
             onChange={(e) => setPost({ ...post, tag: e.target.value })}
-            type="text"
-            placeholder="#Tag"
+            type='text'
+            placeholder='#Tag'
             required
-            className="form_input"
+            className='form_input'
           />
         </label>
 
-        <div className="flex-end mx-3 mb-5 gap-4">
-          <Link href="/" className="text-gray-500 text-sm">
+        <div className='flex-end mx-3 mb-5 gap-4'>
+          <Link href='/' className='text-gray-500 text-sm'>
             Cancel
           </Link>
 
           <button
-            type="submit"
+            type='submit'
             disabled={submitting}
-            className="px-5 py-1.5 text-sm submit_btn rounded-full text-white"
+            className='px-5 py-1.5 text-sm submit_btn rounded-full text-white'
           >
             {submitting ? `${type}ing...` : type}
           </button>
